6d9eb348dbe27d30230bc3a77ba8632086a9d866
Rijan25 <rijan07pokhrel@gmail.com>
2025-10-08 12:18:09 +0545

    feat(langchain): explore SystemMessage, AIMessage, and HumanMessage

---
+++ Langchain/4.Messages/messages.py
@@ -0,0 +1,39 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key=os.getenv('GOOGLE_API_KEY')
+model=ChatGoogleGenerativeAI(model='gemini-2.5-flash-lite-preview-09-2025',temperature=0.1,api_key=api_key)
+
+# Types of Messages
+# 1.System Message : It is the initial prompt that we give to the LLM at the beginings.
+# 2.Human Message : ThE queries that the human gives to the LLMs.
+# 3.AI Message : The response or message the LLM replies.
+
+messages=[
+    SystemMessage(content='You are the helpful Assistant.'),
+    HumanMessage(content='Who is the PM of India?')
+]
+
+result=model.invoke(messages)
+messages.append(AIMessage(content=result.content))
+print(messages[2].content)
+
+
+
+
+# Creating a simple chatbot
+# chat_history=[]
+# while True:
+#     user_input=input('User:')
+#     chat_history.append(user_input)
+#     if user_input=='exit':
+#         break
+#     result=model.invoke(chat_history)
+#     chat_history.append(result.content)
+#     print(f'AI:{result.content}')
+
+# print(chat_history)
