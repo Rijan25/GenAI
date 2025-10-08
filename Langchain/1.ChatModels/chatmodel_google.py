af918e1d421f5e9c8d87a51591e9ef7930554d66
Rijan25 <rijan07pokhrel@gmail.com>
2025-10-08 12:23:49 +0545

    refactor(chatmodels): update google chat model logic

--- Langchain/1.ChatModels/chatmodel_google.py
+++ Langchain/1.ChatModels/chatmodel_google.py
@@ -5,6 +5,6 @@ import os
 load_dotenv()
 
 api_key=os.getenv('GOOGLE_API_KEY')
-model=ChatGoogleGenerativeAI(model='gemini-2.5-flash',temperature=1,api_key=api_key)
-result=model.invoke('Who is Narendra Modi?')
-print(result.content)
+model=ChatGoogleGenerativeAI(model='gemini-2.5-flash-lite-preview-09-2025',temperature=0.1,api_key=api_key)
+response=model.invoke('Give a short joke on democracy')
+print(response.content)
